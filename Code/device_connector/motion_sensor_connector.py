b1a3e05b170189b05fb854cd655d9d12746fee11
Pergo01 <alessandropergolizzi01@gmail.com>
2024-08-20 11:30:56 +0200

    some connector files have beed renamed. Change of motion_sensor_connector topics and message with senml structure

---
+++ Code/device_connector/motion_sensor_connector.py
@@ -0,0 +1,68 @@
+from gpiozero import MotionSensor
+from Libraries import Publisher
+import time
+import json
+import datetime
+
+
+class PIRSensor:
+
+    def __init__(self, clientID, broker, port):
+        self.motion_sensor = MotionSensor(14)
+        self.broker = broker
+        self.port = port
+        self.client = Publisher(clientID, broker, port, self)
+
+    def start(self):
+        self.client.start()
+        time.sleep(1)
+
+    def publish(self, topic, message, QoS):
+        self.client.publish(topic, message, QoS)
+
+    def stop(self):
+        self.client.stop()
+
+
+if __name__ == "__main__":
+    settings = json.load(open("../mqtt_settings.json"))
+    motion_sensor = PIRSensor("MotionSensor", settings["broker"], settings["port"])
+    motion_sensor.start()
+
+    while True:
+        try:
+            motion_sensor.motion_sensor.wait_for_motion()
+            motion_sensor.publish(
+                settings["baseTopic"] + "/kennel1/sensors/motion",
+                {
+                    "bn": "MotionSensor",
+                    "e": [
+                        {
+                            "n": "motion",
+                            "u": "bool",
+                            "t": datetime.datetime.now,
+                            "v": True,
+                        }
+                    ],
+                },
+                2,
+            )
+            motion_sensor.motion_sensor.wait_for_no_motion()
+            motion_sensor.publish(
+                settings["baseTopic"] + "/kennel1/sensors/motion",
+                {
+                    "bn": "MotionSensor",
+                    "e": [
+                        {
+                            "n": "motion",
+                            "u": "bool",
+                            "t": datetime.datetime.now,
+                            "v": False,
+                        }
+                    ],
+                },
+                2,
+            )
+        except KeyboardInterrupt:
+            break
+    motion_sensor.stop()
